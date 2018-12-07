83267a39fe1a59b3d40855d32bb3932e402f7d53
Timur Duehr <tduehr@gmail.com>
2018-12-07 17:08:04 -0600

    mail category -> tag

--- support/cas-server-support-pm-webflow/src/test/java/org/apereo/cas/pm/web/flow/actions/VerifyPasswordResetRequestActionTests.java
+++ support/cas-server-support-pm-webflow/src/test/java/org/apereo/cas/pm/web/flow/actions/VerifyPasswordResetRequestActionTests.java
@@ -1,6 +1,5 @@
 package org.apereo.cas.pm.web.flow.actions;
 
-import org.apereo.cas.category.MailCategory;
 import org.apereo.cas.util.HttpRequestUtils;
 import org.apereo.cas.util.junit.EnabledIfContinuousIntegration;
 import org.apereo.cas.util.junit.EnabledIfPortOpen;
@@ -8,7 +7,7 @@ import org.apereo.cas.util.junit.EnabledIfPortOpen;
 import lombok.val;
 import org.apereo.inspektr.common.web.ClientInfo;
 import org.apereo.inspektr.common.web.ClientInfoHolder;
-import org.junit.experimental.categories.Category;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
@@ -26,7 +25,7 @@ import static org.junit.jupiter.api.Assertions.*;
  */
 @EnabledIfContinuousIntegration
 @EnabledIfPortOpen(port = 25000)
-@Category(MailCategory.class)
+@Tag("mail")
 public class VerifyPasswordResetRequestActionTests extends BasePasswordManagementActionTests {
     @Test
     public void verifyAction() {
